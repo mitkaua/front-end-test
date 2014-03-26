b11336e1a05829c75ea0561098383b2665544f9e
dimon <mitkaua@gmail.com>
2014-03-26 18:36:02 +0200

    improdev styles

--- test-edit/js/file.js
+++ test-edit/js/file.js
@@ -5,3 +5,15 @@ $(document).ready(function(){
         pager:  '#nav'
     });
 });
+
+$(document).ready(function(){
+    $('#ddmenu li').hover(function () {
+        clearTimeout($.data(this,'timer'));
+        $('ul',this).stop(true,true).slideDown(200);
+    }, function () {
+        $.data(this,'timer', setTimeout($.proxy(function() {
+            $('ul',this).stop(true,true).slideUp(200);
+        }, this), 100));
+    });
+
+});
